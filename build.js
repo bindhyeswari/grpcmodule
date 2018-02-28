b43c0ca837d1cd6097b26141b7b5ee7c550bfd44
Bindhyeswari Prasad Mishra <bmishra@netflix.com>
2018-02-28 15:07:25 -0800

    next

--- build.js
+++ build.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
 const packageJson = require('./package.json');
-packageJson.version = '0.0.1-rc8';
 
 const exec = require('child_process');
 exec('git describe --tags', (error, stdout, stderr) => {
@@ -11,8 +10,10 @@ exec('git describe --tags', (error, stdout, stderr) => {
   }
   console.log(`stdout: ${stdout}`);
 console.log(`stderr: ${stderr}`);
+  packageJson.version = stdout;
+  fs.writeFileSync('./package.json', JSON.stringify(packageJson));
+
 });
 
 //todo: path resolve and dirname
-fs.writeFileSync('./package.json', JSON.stringify(packageJson));
 
